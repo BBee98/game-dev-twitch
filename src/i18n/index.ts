d19cafd4cb5a142e6db3a612178ae1e0efecf7e6
lidiasanchez <blue98.bee.dev@gmail.com>
2025-04-10 17:36:08 +0200

    created translate files

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -2,13 +2,12 @@ import i18next from 'i18next';
 import resourcesToBackend from "i18next-resources-to-backend";
 
 i18next
-    .use(resourcesToBackend((language: string, namespace: string) => import(`./${language}/${namespace}.json`)))
+    .use(resourcesToBackend(
+        (language: string, namespace: string) => import(`./${language}/${namespace}.json`)))
     .init({
-    fallbackLng: 'en',
-    ns: ['file1', 'file2'],
-    defaultNS: 'file1',
+    fallbackLng: 'en-GB',
+    ns: ['common'],
+    defaultNS: 'common',
     lng: 'es-ES',
-
-}, () => {
 
 });
